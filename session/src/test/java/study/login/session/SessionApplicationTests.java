929442e759f6c5727b25bd9ab8900bdd0556d0fc
iceprins <junghankyul@gmail.com>
2025-03-03 18:05:09 +0900

    initial commit

---
+++ session/src/test/java/study/login/session/SessionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.login.session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
