047a29104e8bd5f7cd42b03e2ccede51cab7c6f9
iceprins <junghankyul@gmail.com>
2025-05-20 23:43:34 +0900

    feat: 개인정보 암호화 기능 추가에 따른 에러코드 추가

--- session/src/main/java/study/login/session/global/exception/ErrorCode.java
+++ session/src/main/java/study/login/session/global/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ErrorCode {
     CONFLICT_MEMBER(409, "이미 사용 중인 이메일입니다."),
     UNAUTHORIZED_MEMBER(401, "로그인 정보가 잘못되었습니다."),
+    ENCRYPTION_ERROR(500, "암호화 처리 중 에러가 발생했습니다."),
     INTERNAL_ERROR(500, "서버에서 에러가 발생했습니다.");
 
     private final int code;
